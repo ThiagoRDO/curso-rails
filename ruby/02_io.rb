44b71fe267e9c923f194dc4252ee83e84e2fca16
ThiagoRDO <thiagodaqui@gmail.com>
2019-12-11 23:38:32 +0000

    02 e 03

---
+++ ruby/02_io.rb
@@ -0,0 +1,10 @@
+puts "Seu nome:"
+nome = gets.chomp
+puts "Nome e: " + nome
+
+puts  "com inspect " + nome.inspect
+
+puts "digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "sua idade eh: " + idade.to_s
