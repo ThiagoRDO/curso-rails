db807deb01cae980b7a5ddf064a45cacdc3bfbd0
ThiagoRDO <thiagodaqui@gmail.com>
2019-12-17 19:58:00 -0300

    exemplos de estruturas condicionais

--- ruby/03_condicionais.rb
+++ ruby/03_condicionais.rb
@@ -4,3 +4,35 @@ puts 'entre com um numero: '
 x = gets.chomp.to_i
 
 puts 'maior que dois' if x > 2
+
+# outro modo
+
+if x > 2
+  puts 'maior que dois'
+end
+
+# uso do unless
+
+unless x >= 2
+  puts 'menor que dois'
+else
+  puts 'maior que dois'
+end
+
+# uso do case
+
+idade = 5
+case idade
+when 0..2
+  puts 'bebe'
+when 3..12
+  puts 'crianca'
+when 13..18
+  puts 'adolescente'
+else
+  puts 'adulto'
+end
+
+# uso do ternario
+
+puts(x > 10 ? 'maior que dez' : 'menor que dez.')
