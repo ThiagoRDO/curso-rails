c233e48876338250d069a82a951a9f8affeb472b
ThiagoRDO <thiagodaqui@gmail.com>
2019-12-17 19:42:40 -0300

    ajustes do rubocop e solargraph no vscode

--- ruby/02_io.rb
+++ ruby/02_io.rb
@@ -1,10 +1,12 @@
-puts "Seu nome:"
+# frozen_string_literal: true
+
+puts 'Seu nome:'
 nome = gets.chomp
-puts "Nome e: " + nome
+puts 'Nome e: ' + nome
 
-puts  "com inspect " + nome.inspect
+puts 'com inspect ' + nome.inspect
 
-puts "digite sua idade: "
+puts 'digite sua idade: '
 idade = gets.chomp.to_i
 
-puts "sua idade eh: " + idade.to_s
+puts 'sua idade eh: ' + idade.to_s
