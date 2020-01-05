38491b2888c73a4ddf858c32d638757d51bb61be
ThiagoRDO <thiagodaqui@gmail.com>
2020-01-05 15:32:57 -0300

    ainda sobre classes, metodos e o initializa

--- 02_modulo/13_pessoa.rb
+++ 02_modulo/13_pessoa.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 # Classe Pessoa para exemplo
+# com uso de metodo, initialize
+# e passagem de parametro
 class Pessoa
-  def falar
-    'ola mundo'
+  # metodo de inicializacao da classe
+  # nao eh obrigatorio
+  def initialize
+    puts 'nasceu uma pessoa!'
+  end
+  # uso de param = something
+  # valor padrao caso seja null, etc
+  def falar(msg = 'pessoal!')
+    "ola, #{msg}"
   end
 end
